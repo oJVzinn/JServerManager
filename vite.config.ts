16fa87eb0e1b734a826a7c271e6cbc8b7a65404d
ojvzinn <joaovictor17082006@gmail.com>
2026-09-03 23:28:47 -0300

    Inicializado o projeto

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Electron loads the production renderer from a file:// URL.
   base: './',
   plugins: [react()],
 })
